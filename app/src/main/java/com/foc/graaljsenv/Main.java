ba82001fe77f549a8cf950d233030b8f9ae36f13
Mythical Forest Collective <Random.Access.Exomemory@gmail.com>
2022-02-23 22:12:57 +0000

    Add working implementation

--- app/src/main/java/com/foc/graaljsenv/Main.java
+++ app/src/main/java/com/foc/graaljsenv/Main.java
@@ -3,8 +3,38 @@
  */
 package com.foc.graaljsenv;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+import org.graalvm.polyglot.Engine;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.HostAccess;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        if (args.length != 1) {
+            System.out.println("Can only accept one argument!");
+            return;
+        }
+
+        String code;
+        try {
+            code = Files.readString(Paths.get(args[0]));
+        } catch (IOException e) {
+            System.out.println("Can't find the specified file!");
+            return;
+        }
+
+        Engine engine = Engine.newBuilder()
+          .option("engine.WarnInterpreterOnly", "false")
+          .build();
+
+        Context context = Context.newBuilder("js")
+         .allowHostAccess(HostAccess.ALL)
+         .allowHostClassLookup(className -> true)
+         .engine(engine)
+         .build();
+        context.eval("js", code);
     }
 }
